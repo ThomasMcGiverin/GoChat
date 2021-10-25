1f73b27aa328b6517b9b8871667051013c8e6c73
thomas mcgiverin <thomasmcgiverin@hotmail.com>
2021-10-24 23:47:38 -0400

    Added go backend and jquery frontend

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	rdb *redis.Client
+
+	clients = make(map[*websocket.Conn]bool)
+	broadcaster = make(chan ChatMessage)
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+type ChatMessage struct {
+	Username string `json:"username"`
+	Text string `json:"text"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("woooo")
+	defer ws.Close()
+	clients[ws] = true
+
+	if rdb.Exists("chat_messages").Val() != 0 {
+		sendPreviousMessages(ws)
+	}
+
+	for {
+		var msg ChatMessage
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		broadcaster <- msg
+	}
+}
+
+func sendPreviousMessages(ws *websocket.Conn) {
+	chatMessages, err := rdb.LRange("chat_messages", 0, -1).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, chatMessage := range chatMessages {
+		var msg ChatMessage
+		json.Unmarshal([]byte(chatMessage), &msg)
+		messageClient(ws, msg)
+	}
+}
+
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
+}
+
+func handleMessages() {
+	for {
+		msg := <- broadcaster
+
+		storeInRedis(msg)
+		messageClients(msg)
+	}
+}
+
+func storeInRedis(msg ChatMessage) {
+	json, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	err = rdb.RPush("chat_messages", json).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func messageClients(msg ChatMessage) {
+	for client := range clients {
+		messageClient(client, msg)
+	}
+}
+
+func messageClient(client *websocket.Conn, msg ChatMessage) {
+	err := client.WriteJSON(msg)
+	if err != nil && unsafeError(err) {
+		log.Printf("error: %v", err)
+		client.Close()
+		delete(clients, client)
+	}
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	redisURL := os.Getenv("REDIS_URL")
+	opt, err := redis.ParseURL(redisURL)
+	if err != nil {
+		panic(err)
+	}
+	rdb = redis.NewClient(opt)
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/websocket", handleConnections)
+	go handleMessages()
+
+	log.Print("Server started at localhost:4444")
+
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
